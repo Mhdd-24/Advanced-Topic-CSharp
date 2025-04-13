22778cf50a12f10b7fa3065d455db0f7bbe0c599
Mohammed Rafi M <mhdd24@Mohammeds-MacBook-Air.local>
2025-04-13 16:21:13 +0530

    first commit

---
+++ PrintAll/CustomPrint.cs
@@ -0,0 +1,35 @@
+﻿namespace PrintAll;
+
+public class CustomPrint
+{
+    private string name;
+
+    public void Print()
+    {
+        Console.WriteLine("Printing from Print");
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void PrintName()
+    {
+        Console.WriteLine($"Name set as {name}");
+    }
+
+    public void Print(string name)
+    {
+        Console.WriteLine($"Name Passed: {name}");
+    }
+
+    private void PrintPrivate()
+    {
+        Console.WriteLine("Printing from private");
+    }
+
+    public string Name => name;
+
+    public static string StaticName => "Static Property Name";
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace MyApp
+{
+    internal class Reflection
+    {
+        static void Main(string[] args)
+        {
+            var assembly = Assembly.LoadFrom(@"Tutorial-Reflection/PrintAll/bin/Debug/net9.0/PrintAll.dll");
+
+            foreach (var type in assembly.GetTypes())  
+            {
+                Console.WriteLine($"Type: {type.Name}");
+                Console.WriteLine("------------------");
+                var instance = Activator.CreateInstance(type)
+
+                foreach (var field in type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+                {
+                     Console.WriteLine($"Field: {field.Name}");
+                    //  field.SetValue(instance, "Frdo")
+                }
+                 Console.WriteLine("------------------");
+
+                 foreach (var methods in type.GetMethods(BindingFlags.Public |BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly).Where(m => m.IsSpecialName))
+                 {
+                    Console.WriteLine($"Methods: {methods.Name}");
+                    if(method.GetParameters().length > 0)
+                    {
+                        method.Invoke(instance, new[] {'Biblo'})
+                    }
+                    else if(method.ReturnType.Name != "Void")
+                    {
+                        var returnedValue = methods.Invoke(instance, null);
+                        Console.WriteLine($"Returned valur form method: {returnedValue}");
+                    }
+                    else{
+                         methods.Invoke(instance, null);
+                    }
+                   
+                 }
+                  Console.WriteLine("------------------");
+                  foreach (var property in type.GetProperties())
+                  {
+                    Console.WriteLine($"Property: {property.Name}");
+                    var propertyValue = property.GetValue(instance);
+                    Console.WriteLine($"Property Value: {propertyValue}");
+                  }
+            }
+        }
+    }
+}
