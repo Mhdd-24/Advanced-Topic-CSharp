fc3e525210af87c960b7deb9f965b964497ddf5a
Mohammed Rafi <105036633+Mhdd-24@users.noreply.github.com>
2025-04-13 16:33:21 +0530

    Add files via upload

---
+++ Tutorial-Reflection/PrintAll/CustomPrint.cs
@@ -0,0 +1,35 @@
+﻿namespace PrintAll;
+
+public class CustomPrint
+{
+    private string name;
+
+    public void Print()
+    {
+        Console.WriteLine("Printing from Print");
+    }
+
+    public string GetName()
+    {
+        return this.name;
+    }
+
+    public void PrintName()
+    {
+        Console.WriteLine($"Name set as {this.name}");
+    }
+
+    public void Print(string name)
+    {
+        Console.WriteLine($"Name Passed: {name}");
+    }
+
+    private void PrintPrivate()
+    {
+        Console.WriteLine("Printing from private");
+    }
+
+    public string Name => name;
+
+    public static string StaticName => "Static Property Name";
+}
