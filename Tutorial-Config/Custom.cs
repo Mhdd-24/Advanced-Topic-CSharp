67503903cce3f0bab38b1e95ad2e31805a54786b
Mhdd-24 <rafirahmath2@gmail.com>
2025-04-14 04:27:32 +0530

    Refactor project structure: implement Program and Startup classes, add custom configuration support, and enhance appsettings

---
+++ Tutorial-Config/Custom.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorial_Config
+{
+    public class Custom
+    {
+        public string ConfigValue { get; set; }
+    }
+}
---
+++ Tutorial-Config/Settings.cs
@@ -0,0 +1,7 @@
+namespace Tutorial_Config
+{
+    public class Settings
+    {
+        public string Database { get; set; }
+    }
+}
